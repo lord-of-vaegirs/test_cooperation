593730f48f7f743d106b3aaaa702a5eb3d0f1449
lord-of-vaegirs <lxj133_1226@outlook.com>
2025-11-21 16:03:12 +0800

    finish the branch of feature/lxjdev

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,5 +2,10 @@
 
 int main(){
     std::cout << "Hello, World!" << std::endl;
+    int sum=0;
+    for(int i=1; i<=10; ++i){
+        sum += i;
+    }
+    std::cout << "Sum of first 10 natural numbers is: " << sum << std::endl;
     return 0;
 }
--- python_framework.py
+++ python_framework.py
@@ -2,3 +2,4 @@ n=[1, 2, 3]
 for i in n:
     print(i)
 print("Done")
+print("Sum of first 10 natural numbers is: 55")
